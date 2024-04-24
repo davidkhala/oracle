689a56303264c51b02d7ebf81b54c6499a60d4be
David Liu <david-khala@hotmail.com>
2024-04-24 06:19:54 +0000

    make it runnable

--- goldengate/install/oracle-db.sh
+++ goldengate/install/oracle-db.sh
@@ -1,2 +1,4 @@
-
-./fbo_ggs_Linux_x64_Oracle_shiphome/Disk1/runInstaller -silent -nowait -responseFile ./fbo_ggs_Linux_x64_Oracle_shiphome/Disk1/response/oggcore.rsp
+set -e
+java -version # require java
+sudo dnf install -y libnsl
+./fbo_ggs_Linux_x64_Oracle_shiphome/Disk1/runInstaller -silent -nowait -responseFile $PWD/fbo_ggs_Linux_x64_Oracle_shiphome/Disk1/response/oggcore.rsp
