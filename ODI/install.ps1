74dccc5b49e277ad826433c884554f41285b5f4b
David Liu <david-khala@hotmail.com>
2024-05-22 10:44:29 +0800

    Update install.ps1

--- ODI/install.ps1
+++ ODI/install.ps1
@@ -1,3 +1,3 @@
 
--o odi.jar https://cn9yc2hk0gzg.objectstorage.ap-singapore-1.oci.customer-oci.com/n/cn9yc2hk0gzg/b/e-delivery/o/fmw_12.2.1.4.0_odi.jar # TODO get 
+Invoke-WebRequest -Uri https://cn9yc2hk0gzg.objectstorage.ap-singapore-1.oci.customer-oci.com/n/cn9yc2hk0gzg/b/e-delivery/o/fmw_12.2.1.4.0_odi.jar -OutFile odi.jar
 & 'C:\Program Files\Java\jdk-1.8\bin\java' -jar .\odi.jar
